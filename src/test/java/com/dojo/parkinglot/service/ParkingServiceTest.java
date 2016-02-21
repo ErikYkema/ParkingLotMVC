044a7778d46340d1d16346e5e45764b23d2ac11f
Oscar Riezebos <oriezebos@bol.com>
2016-02-21 01:29:06 +0100

    SP07 Junit tests

--- src/test/java/com/dojo/parkinglot/service/ParkingServiceTest.java
+++ src/test/java/com/dojo/parkinglot/service/ParkingServiceTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.lang.invoke.MethodHandles;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,9 +19,12 @@ public class ParkingServiceTest {
     private final static Logger LOG =
             LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    @Autowired
+    ParkingServiceInterface parkingService;
+
     @Test
-    public void test() {
-        assertTrue(true);
+    public void testParkinglot() {
+        assertNotNull(parkingService.getParkingLot());
     }
 
 }
